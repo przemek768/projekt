6098c3df7667c2f99443684e9ce0b11b7cb7ebd6
przemek768 <przemek76816@gmail.com>
2023-01-07 15:27:51 +0100

    new

---
+++ var/cache/dev/url_matching_routes.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/main/site' => [[['_route' => 'app_main_site', '_controller' => 'App\\Controller\\MainSiteController::index'], null, null, null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        35 => [
+            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
