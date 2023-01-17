e94a73f8ae62e09361c002520f42cc7a51db5ad5
przemek768 <przemek76816@gmail.com>
2023-01-17 18:50:52 +0100

    s

--- sql/register.php
+++ sql/register.php
@@ -1,34 +1,21 @@
 <?php
 include ('../DataBaseConnection.php');
 
-$data = array([
-    'user_name' => $_POST['firstName'],
-    'user_lastname' => $_POST['lastName'],
-    'password' => $_POST['password'],
-    'password_confirm' => $_POST['passwordConfirm'],
-    'email' => $_POST['email'],
-    'email_confirm' => $_POST['email_confirm'],
-    'phone_number' => $_POST['phone_number'],
-    'date_of_birth' => $_POST['date']
-]);
-$user_name = $data[0]['user_name'];
-$user_lastname = $data[0]['user_lastname'];
-$password = $data[0]['password'];
-$password_confirm = $data[0]['password_confirm'];
-$email = $data[0]['email'];
-$email_confirm = $data[0]['email_confirm'];
-$phone_number = $data[0]['phone_number'];
-$date_of_birth = $data[0]['date_of_birth'];
+$user_name = $_POST['firstName'];
+$user_surname = $_POST['lastName'];
+$user_password = $_POST['password'];
+$user_password_confirm = $_POST['passwordConfirm'];
+$user_email = $_POST['email'];
+$user_email_confirm = $_POST['email_confirm'];
+$user_phone_number = $_POST['phone_number'];
+$user_date = $_POST['date'];
 
-if($data){
-    if($data['password'] == $data['passwordConfirm'] && $data['email'] == $data['email_confirm']){
-        $sql = "INSERT INTO users (user_name, user_lastname, password, email, phone_number, date_of_birth) VALUES ($user_name, $user_lastname, $password, $email, $phone_number, $date_of_birth)";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute($data);
-        header('Location: ../index.php');
-    }
-    else{
-        echo "Hasła nie są takie same";
-    }
+
+if($user_password == $user_password_confirm && $user_email == $user_email_confirm) {
+    $sql = "INSERT INTO users (user_name, user_lastname, password, email, phone_number, birth_date) VALUES ('$user_name', '$user_surname', '$user_password', '$user_email', '$user_phone_number', '$user_date')";
+    $pdo->exec($sql);
+    header('Location: ../index.php');
+} else {
+    echo "Hasła nie są takie same lub email nie jest taki sam";
 }
 
