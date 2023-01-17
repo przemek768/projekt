0dcf545e14f51d3f4bdedb0aac18a7a2dcb92adb
przemek768 <przemek76816@gmail.com>
2023-01-17 18:59:13 +0100

    s

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php include('Templates/header.php') ?>
+<main class="page lanidng-page"></main>
+<div class="login-clean">
+    <form method="post" action="sql/login.php">
+        <h2 class="sr-only">Login Form</h2>
+        <div class="illustration" style="color: rgb(71,119,244);"><i class="icon ion-ios-navigate"></i></div>
+        <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email"></div>
+        <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+        <div class="form-group"><a class="btn btn-primary btn-block" role="button" type="submit" style="background-color: rgb(71,119,244);">Log In</a></div>
+    </form>
+</div>
+<?php include('Templates/footer.php') ?>
---
+++ sql/login.php
@@ -0,0 +1,26 @@
+<?php
+include ('../DataBaseConnection.php');
+
+session_start();
+
+if(isset($_SESSION['user']) && !empty($_SESSION['user'])) {
+    header('Location: ../client_panel.php');
+    exit;
+}
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+$result = $pdo->query($sql);
+if($result){
+    $user = $result->fetch();
+    if($user){
+        $_SESSION['user'] = true;
+        header('Location: ../client_panel.php');
+    } else {
+        echo "Niepoprawny email lub hasło";
+    }
+} else {
+    echo "Błąd bazy danych";
+}
