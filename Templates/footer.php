6614eaae40ace6ebe00baab90104a4b5e40c5c69
przemek768 <przemek76816@gmail.com>
2023-01-21 12:38:11 +0100

    s

--- Templates/footer.php
+++ Templates/footer.php
@@ -4,11 +4,13 @@
         <div class="social-icons"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-instagram-outline"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a></div>
     </div>
 </footer>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script src="assets/js/jquery.min.js"></script>
 <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/pikaday.min.js"></script>
 <script src="assets/js/theme.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+<script src="assets/js/agent.js"></script>
+
 </body>
 
 </html>
--- agent.php
+++ agent.php
@@ -46,35 +46,4 @@ foreach ($agents_clients as $agents_client){
         </div>
     </div>
 </div>
-<script>
-    $(document).ready(function(){
-       $('select.client').change(function(){
-           var client_id = $(this).val();
-           $.ajax({
-               url: 'sql/show_offers.php',
-               type: 'POST',
-               data: {client_id: client_id},
-               success: function(data){
-                     $('.change').html();
-                        $('.change').html(data);
-               }
-           });
-       });
-        $(document).on('click', '.btn-primary', function(){
-            var clients = $('#clients').val();
-            var agents = $('#agents').val();
-            fd = new FormData();
-            fd.append('clients', clients);
-            fd.append('agents', agents);
-            $.ajax({
-                url: 'sql/add_offer.php',
-                type: 'POST',
-                data: fd,
-                success: function(data){
-                    alert(data);
-                }
-            });
-        });
-    });
-</script>
 <?php include 'Templates/footer.php'; ?>
