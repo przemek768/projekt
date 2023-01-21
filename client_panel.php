5722fefd0f3184352da9f0fc130555b83e5e0528
przemek768 <przemek76816@gmail.com>
2023-01-21 10:41:19 +0100

    s

--- client_panel.php
+++ client_panel.php
@@ -15,8 +15,25 @@
                 <h1>Wymagania</h1>
                 <?php
                 $wymagania = $pdo->query("SELECT * FROM requirements WHERE user_id = '$user_id'");
-                ?>
-                <form method="post" action="sql/wymagania.php">
+                $wymagania = $wymagania->fetchAll();
+                $wymagania_count = count($wymagania);
+
+                if($wymagania_count > 0){
+                    echo'
+                        <form method="post" action="sql/wymagania_update.php">
+                    <div class="form-group"><label for="subject">Rodzaj nieruchomości</label><select class="form-control" name="subject" id="subject"><option value="gruntowe">gruntowe</option><option value="lokalowe">lokalowe</option><option value="budynkowe">budynkowe</option></select></div>
+                    <div
+                        class="form-group"><label>Lokalizacja</label><input name="localization" id="localization" class="form-control" type="text"></div>
+                    <div class="form-group"><label for="email">Minimalny rozmiar<br></label><input id="min-size" name="min-size" class="form-control" type="number" name="size" min="5" max="5000" step="1"></div>
+                    <div class="form-group"><label for="email">Minimalna liczba pokoi<br></label><input id="min-room-number" name="min-room-number" class="form-control" type="number" name="size" min="1" max="999" step="1"></div>
+                    <div class="form-group"><label for="email">Maksymalna cena<br></label><input name="max-price" id="max-price" class="form-control" type="number" name="size" min="1" max="9999999999" step="1000"></div>
+                    <input type="hidden" name="user_id" value="'.$user_id.'">
+                    <div class="col d-xl-flex justify-content-xl-center align-items-xl-end"><button class="btn btn-primary btn-block btn-lg text-center d-xl-flex justify-content-xl-center" type="submit">Zatwierdź wymagania</button></div>
+                </form>
+                    ';
+                }else{
+                    echo'
+                    <form method="post" action="sql/wymagania.php">
                     <div class="form-group"><label for="subject">Rodzaj nieruchomości</label><select class="form-control" name="subject" id="subject"><option value="gruntowe">gruntowe</option><option value="lokalowe">lokalowe</option><option value="budynkowe">budynkowe</option></select></div>
                     <div
                         class="form-group"><label>Lokalizacja</label><input name="localization" id="localization" class="form-control" type="text"></div>
@@ -24,7 +41,9 @@
                     <div class="form-group"><label for="email">Minimalna liczba pokoi<br></label><input id="min-room-number" name="min-room-number" class="form-control" type="number" name="size" min="1" max="999" step="1"></div>
                     <div class="form-group"><label for="email">Maksymalna cena<br></label><input name="max-price" id="max-price" class="form-control" type="number" name="size" min="1" max="9999999999" step="1000"></div>
                     <div class="col d-xl-flex justify-content-xl-center align-items-xl-end"><button class="btn btn-primary btn-block btn-lg text-center d-xl-flex justify-content-xl-center" type="submit">Zatwierdź wymagania</button></div>
-                </form>
+                </form>';
+                }
+                ?>
             </div>
             <div class="col-md-12 col-xl-6">
                 <h1>Oferty</h1>
---
+++ sql/wymagania_update.php
@@ -0,0 +1,11 @@
+<?php
+include '../DataBaseConnection.php';
+$subject = $_POST['subject'];
+$min_size = $_POST['min-size'];
+$min_room_number = $_POST['min-room-number'];
+$max_price = $_POST['max-price'];
+$localization = $_POST['localization'];
+$user_id = $_SESSION['user_id'];
+$sql = ("UPDATE requirements SET subject = '$subject', min_size = '$min_size', min_room_number = '$min_room_number', max_price = '$max_price', localization = '$localization' WHERE user_id = '$user_id'");
+$pdo->query($sql);
+header('Location: ../client_panel.php');
