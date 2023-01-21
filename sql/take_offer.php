ce6e06d767bd9ca0c35957eb957660220694db05
przemek768 <przemek76816@gmail.com>
2023-01-21 20:19:39 +0100

    s

--- sql/take_offer.php
+++ sql/take_offer.php
@@ -2,3 +2,4 @@
 include '../DataBaseConnection.php';
 $id_clients_offers = $_POST['id_clients_offers'];
 $pdo->query("UPDATE clients_offers SET status = 1 WHERE id_clients_offers = '$id_clients_offers'");
+header('Location: ../agent.php');
