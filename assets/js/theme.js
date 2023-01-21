63bd64b07eadcbd2d762ea4fe7b9a44c7fdb1231
przemek768 <przemek76816@gmail.com>
2023-01-21 11:10:51 +0100

    s

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,26 +1,17 @@
+
 $('.datepicker').each(function(){
 	var picker = new Pikaday({
 		field: this
 	});
 });
-const swiper = new Swiper('.swiper', {
-	// Optional parameters
-	direction: 'vertical',
-	loop: true,
-
-	// If we need pagination
-	pagination: {
-		el: '.swiper-pagination',
-	},
-
-	// Navigation arrows
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-
-	// And if we need scrollbar
-	scrollbar: {
-		el: '.swiper-scrollbar',
-	},
+$(document).ready(function(){
+	let clients_swiper = new Swiper('.swiper', {
+		direction: "horizontal",
+		slidesPerView: 1,
+		loop: true,
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	});
 });
