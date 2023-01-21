e63deea2fd36ecf8dc30debfaa5f2e1fb6f3c640
przemek768 <przemek76816@gmail.com>
2023-01-21 20:15:25 +0100

    s

--- client_panel.php
+++ client_panel.php
@@ -1,5 +1,8 @@
 <?php include 'Templates/header.php'; ?>
 <?php session_start(); ?>
+<?php
+$cliet_offers = $pdo->query("SELECT * FROM clients_offers INNER JOIN offers ON clients_offers.offer_id = offers.offer_id WHERE clients_offers.client_id = '$user_id'");
+?>>
 <main class="page projects-page">
     <section class="portfolio-block projects-cards">
         <div class="container">
@@ -49,14 +52,41 @@
             </div>
             <div class="col-md-12 col-xl-6">
                 <h1>Oferty</h1>
-                <form>
-                    <h2 class="text-center">Tytuł oferty</h2>
-                    <p class="text-center">Rodzaj nieruchomości: lokalowa</p>
-                    <p class="text-center">Lokalizacja: adres</p>
-                    <p class="text-center">Rozmiar: 20m2</p>
-                    <p class="text-center">Liczba pokoi: 10</p>
-                    <p class="text-center">Cena: 400000 zł</p>
-                    <p class="text-center">Opis: Fajne mieszkanie</p><button class="btn btn-primary text-center d-xl-flex" type="button" style="margin-left: auto;margin-right: auto;">Pokaż więcej</button></form>
+                <div class="swiper">
+                    <!-- Additional required wrapper -->
+                    <div class="swiper-wrapper">
+                        <!-- Slides -->
+                        <?php foreach ($cliet_offers as $offert){
+                            echo '
+                            <div class="swiper-slide">
+                            <form action="">
+                                <h2 class="text-center">'. $offert['subject'] .' ' . $offert['localization'] .'</h2>
+                                <p class="text-center">Rodzaj nieruchomości: '. $offert['subject'] .'</p>
+                                <p class="text-center">Lokalizacja: ' . $offert['localization'] .'</p>
+                                <p class="text-center">Rozmiar: ' . $offert['min_size'] .'</p>
+                                <p class="text-center">Liczba pokoi: ' . $offert['min_room_number'] .'</p>
+                                <p class="text-center">Cena: ' . $offert['max_price'] .' zł</p>
+                                <p class="text-center">Opis: ' . $offert['opis'] .'</p>
+                                <input type="hidden" id="id_clients_offers" name="id_clients_offers" value="'. $offert['id_clients_offers'] .'">
+                                <button class="btn btn-primary" type="submit"></button>
+                                </form>
+                        </div>
+                            ';
+                        }
+
+                        ?>
+                        ...
+                    </div>
+                    <!-- If we need pagination -->
+                    <div class="swiper-pagination"></div>
+
+                    <!-- If we need navigation buttons -->
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+
+                    <!-- If we need scrollbar -->
+                    <div class="swiper-scrollbar"></div>
+                </div>
             </div>
         </div>
     </div>
---
+++ sql/take_offer.php
@@ -0,0 +1,4 @@
+<?php
+include '../DataBaseConnection.php';
+$id_clients_offers = $_POST['id_clients_offers'];
+$pdo->query("UPDATE clients_offers SET status = 1 WHERE id_clients_offers = '$id_clients_offers'");
