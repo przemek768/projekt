67689458ce649c8e186cc5eb95d31cbc63cce206
Damian Zabijak <zymek2k@gmail.com>
2023-01-21 10:45:19 +0100

    d

--- DataBaseConnection.php
+++ DataBaseConnection.php
@@ -1,7 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=database', 'test', '');
+$pdo = new PDO('mysql:host=127.0.0.1:3307;dbname=projekt', 'root', 'root');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-?>
-$pdo = new PDO('mysql:host=127.0.0.1:3307;dbname=projekt', 'root', 'root');
 ?>
--- Templates/header.php
+++ Templates/header.php
@@ -14,15 +14,15 @@ if($_SESSION['user']){
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>CV - Brand</title>
-    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
-    <link rel="stylesheet" href="/assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-    <link rel="stylesheet" href="/assets/css/Article-List.css">
+    <link rel="stylesheet" href="assets/css/Article-List.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/css/pikaday.min.css">
-    <link rel="stylesheet" href="/assets/css/Login-Form-Clean.css">
-    <link rel="stylesheet" href="/assets/css/NavBar-Notifications-Panel-1.css">
-    <link rel="stylesheet" href="/assets/css/NavBar-Notifications-Panel.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/NavBar-Notifications-Panel-1.css">
+    <link rel="stylesheet" href="assets/css/NavBar-Notifications-Panel.css">
 </head>
 
 <body>
