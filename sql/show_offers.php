7a614e559ee6676b72901aacacba2ae6479ed956
przemek768 <przemek76816@gmail.com>
2023-01-21 13:01:33 +0100

    s

--- sql/show_offers.php
+++ sql/show_offers.php
@@ -6,10 +6,16 @@ $wymagania = $wymagania->fetchAll();
 $offers = $pdo->query("SELECT * FROM offers");
 $offers = $offers->fetchAll();
 $check = [];
+$check_client_offers = $pdo->query("SELECT * FROM clients_offers WHERE client_id = '$client_id'");
+$check_client_offers = $check_client_offers->fetchAll();
 foreach ($wymagania as $wymaganie) {
     foreach ($offers as $offer) {
-        if ($wymaganie['subject'] == $offer['subject'] && $wymaganie['min_size'] <= $offer['min_size'] && $wymaganie['min_room_number'] <= $offer['min_room_number'] && $wymaganie['max_price'] >= $offer['max_price'] && $wymaganie['localization'] == $offer['localization']) {
-            $check[] = $offer;
+        foreach ($check_client_offers as $cco){
+            if($cco['offer_id'] != $offer['id']){
+                if ($wymaganie['subject'] == $offer['subject'] && $wymaganie['min_size'] <= $offer['min_size'] && $wymaganie['min_room_number'] <= $offer['min_room_number'] && $wymaganie['max_price'] >= $offer['max_price'] && $wymaganie['localization'] == $offer['localization']) {
+                    $check[] = $offer;
+                }
+            }
         }
     }
 }
