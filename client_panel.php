8317b7004b5728075c796592a81ffc99720a8f78
przemek768 <przemek76816@gmail.com>
2023-01-21 20:20:16 +0100

    s

--- client_panel.php
+++ client_panel.php
@@ -68,7 +68,7 @@ $cliet_offers = $pdo->query("SELECT * FROM clients_offers INNER JOIN offers ON c
                                 <p class="text-center">Cena: ' . $offert['max_price'] .' zł</p>
                                 <p class="text-center">Opis: ' . $offert['opis'] .'</p>
                                 <input type="hidden" id="id_clients_offers" name="id_clients_offers" value="'. $offert['id_clients_offers'] .'">
-                                <p><button class="btn btn-primary" type="submit"></button></p>
+                                <p><button class="btn btn-primary btn-block btn-lg text-center d-xl-flex justify-content-xl-center" type="submit"></button></p>
                                 </form>
                         </div>
                             ';
